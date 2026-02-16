0d77428db958b90d18f0a0f9cb2c061dd6767fda
krishnav kishor <krishnav.k7k7@gmail.com>
2026-02-16 17:28:53 +0530

    2nd commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from "react";
 const Header = () => {
   const [theme, setTheme] = useState("light");
 
-  // Load saved theme on first render
   useEffect(() => {
     const savedTheme = localStorage.getItem("theme") || "light";
     setTheme(savedTheme);
@@ -22,12 +21,17 @@ const Header = () => {
   };
 
   return (
-    <div className="header">
-      <h2>Customer Support AI</h2>
-      <span className="light-text" onClick={toggleTheme} style={{cursor:"pointer"}}>
+    <header className="header">
+      <h1>Customer Support AI</h1>
+
+      <span
+        className="light-text"
+        onClick={toggleTheme}
+        style={{ cursor: "pointer" }}
+      >
         {theme === "light" ? "Light" : "Dark"} ⚙
       </span>
-    </div>
+    </header>
   );
 };
 
